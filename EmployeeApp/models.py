8f5e989d87ac6bc6a368b0a4f1559bd39d9a942d
Rahul Ratan <rahulratan1509@gmail.com>
2023-08-29 13:45:32 +0530

    Fixed duplicate user field in Entry model

--- EmployeeApp/models.py
+++ EmployeeApp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Branch(models.Model):
     name = models.CharField(max_length=100)
@@ -19,3 +21,13 @@ class Employee(models.Model):
     colliery = models.ForeignKey(Colliery, on_delete=models.CASCADE)
     employee_id = models.CharField(max_length=10, unique=True)
     password = models.CharField(max_length=100)
+
+class Entry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    employee_number = models.CharField(max_length=10)
+    designation = models.CharField(max_length=50)
+    last_visit_date = models.DateField()
+    last_pme_date = models.DateField()
+    date_of_birth = models.DateField()
+    timestamp = models.DateTimeField(default=timezone.now)  # Set default value here
