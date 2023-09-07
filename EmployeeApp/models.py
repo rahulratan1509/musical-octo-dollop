9a3a3d0540fb0a88b55e3e7c58bd09e8f3a4d3b0
Rahul Ratan <rahulratan1509@gmail.com>
2023-09-07 16:46:12 +0530

    last vt date and last pme fields are now optional

--- EmployeeApp/models.py
+++ EmployeeApp/models.py
@@ -27,8 +27,8 @@ class Entry(models.Model):
     name = models.CharField(max_length=100)
     employee_number = models.CharField(max_length=10)
     designation = models.CharField(max_length=50)
-    last_vt_date = models.DateField(verbose_name='Last VT Date')
-    last_pme_date = models.DateField()
+    last_vt_date = models.DateField(verbose_name='Last VT Date', null=True, blank=True)  # Make it optional
+    last_pme_date = models.DateField(null=True, blank=True)  # Make it optional
     date_of_birth = models.DateField()
     next_vt_date = models.DateField(null=True, blank=True)
     next_pme_date = models.DateField(null=True, blank=True)
--- EmployeeApp/views.py
+++ EmployeeApp/views.py
@@ -52,14 +52,34 @@ def add_entry(request):
                 entry.last_vt_date
             )
 
+            # Check if last_vt_date is blank and use the value from last_pme_date if available
+            if not entry.last_vt_date and next_pme_date:
+                entry.last_vt_date = next_pme_date
+
+            # Check if last_pme_date is blank and use the value from last_vt_date if available
+            if not entry.last_pme_date and next_vt_date:
+                entry.last_pme_date = next_vt_date
+
             entry.next_vt_date = next_vt_date
             entry.next_pme_date = next_pme_date
 
             entry.save()  # Now save the entry
+
+            # Debugging messages
+            print("Entry saved successfully.")
+            print(f"next_vt_date: {next_vt_date}")
+            print(f"next_pme_date: {next_pme_date}")
+
             return redirect('dashboard')  # Redirect to the dashboard page
+        else:
+            # Debugging message for form validation errors
+            print("Form is not valid. Errors:")
+            print(form.errors)
     else:
         form = EntryForm()
+
     return render(request, 'EmployeeApp/add_entry.html', {'form': form})
+
 
 def calculate_next_dates(dob, last_pme_date, last_vt_date):
     current_year = datetime.datetime.now().year
@@ -80,7 +100,24 @@ def calculate_next_dates(dob, last_pme_date, last_vt_date):
             next_pme_date = last_pme_date + datetime.timedelta(days=5 * 365)  # PME every 5 years
         elif last_pme_date:
             next_pme_date = last_pme_date + datetime.timedelta(days=3 * 365)  # PME every 3 years
-        else:
-            next_pme_date = dob + datetime.timedelta(days=(45 - age - 3) * 365)  # PME after 3 years
+        elif not last_pme_date:  # Handle the case where last_pme_date is not provided
+            next_pme_date = None  # Set to None if not provided
+
+        # Handle the case where last_vt_date is not provided
+        if not last_vt_date:
+            next_vt_date = None  # Set to None if not provided
+
+        # Check if the person will turn 59 in the next year (2024)
+        if age + 1 == 59 and current_year == 2023:
+            # Set VT and PME dates for 2023 (random date and month)
+            next_vt_date = datetime.date(2023, 6, 15)  # Change to your preferred date
+            next_pme_date = datetime.date(2023, 8, 25)  # Change to your preferred date
+
+        # Check if the person is 59 years old and it's 2024
+        if age == 59 and current_year == 2024:
+            # Set VT and PME dates for 2024 (random date and month)
+            next_vt_date = datetime.date(2024, 7, 10)  # Change to your preferred date
+            next_pme_date = datetime.date(2024, 9, 20)  # Change to your preferred date
 
     return next_vt_date, next_pme_date
+
